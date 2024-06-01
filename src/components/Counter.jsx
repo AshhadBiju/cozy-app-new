94b7cf0fb431c3349fe5de369f1eacab327c1854
AshhadBiju <ashhadbiju2000@gmail.com>
2024-06-01 12:40:43 +0530

    social-icons

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,14 @@
 "use client";
 import { useState } from "react";
-import { FaPlus, FaMinus } from "react-icons/fa";
+import Image from "next/image";
+import plus from "@/public/images/icons/plus.svg";
+import minus from "@/public/images/icons/minus.svg";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
   const increment = () => {
     setCount(count + 1);
   };
-
   const decrement = () => {
     if (count > 0) {
       setCount(count - 1);
@@ -18,11 +18,17 @@ const Counter = () => {
   return (
     <div className="flex gap-10 border-[#A2A3B1] border-2 rounded-[0.25rem] w-40 px-4 py-[0.875rem]">
       <button onClick={decrement}>
-        <FaMinus />
+        <Image
+          src={minus}
+          alt="No button"
+          width={34}
+          height={34}
+          className=""
+        />
       </button>
       <span>{count}</span>
       <button onClick={increment}>
-        <FaPlus />
+        <Image src={plus} alt="No button" width={34} height={34} className="" />
       </button>
     </div>
   );
